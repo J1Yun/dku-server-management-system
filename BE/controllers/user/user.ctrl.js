3038968da8a7903260e59c541356ceee773a1f20
zinirun <heojj97@naver.com>
2020-11-16 21:50:23 +0900

    [BE] Update REST Controllers

--- BE/controllers/user/user.ctrl.js
+++ BE/controllers/user/user.ctrl.js
@@ -11,43 +11,39 @@ module.exports = {
             .catch((error) => res.status(409).json({ error })),
 
     post_signin: async (req, res) => {
-        const LOGIN_QUERY =
-            'SELECT id, userId, firstName, lastName FROM users WHERE userId = :userId AND `password`= :password';
-
         const { userId, password } = req.body.user;
         const secret = req.app.get('jwt-secret');
 
-        return await models.sequelize
-            .query(LOGIN_QUERY, { replacements: { userId, password } })
-            .spread(
-                (result) => {
-                    if (result.length > 0) {
-                        const user = result[0];
-                        // Success signin, generate jwt
-                        jwt.sign(
-                            {
-                                ...user,
-                            },
-                            secret,
-                            {
-                                expiresIn: '7d',
-                                issuer: 'sms.dankook.ac.kr',
-                                subject: 'user',
-                            },
-                            (error, token) => {
-                                if (error) res.status(409).json({ error });
-                                // send final user info
-                                res.cookie('x-access-token', token, { httpOnly: true });
-                                res.json({ message: 'Sign-in Successfully', token });
-                            },
-                        );
-                    } else {
-                        res.status(409).json({ error: { name: 'invalid' } });
-                    }
-                },
-                (error) => {
-                    res.status(409).json({ error });
-                },
-            );
+        return await models.user
+            .findOne({
+                attributes: ['id', 'userId', 'name', 'department', 'tel', 'penalty'],
+                where: { userId, password },
+                raw: true,
+            })
+            .then((user) => {
+                if (user) {
+                    // Success signin, generate jwt
+                    jwt.sign(
+                        {
+                            ...user,
+                        },
+                        secret,
+                        {
+                            expiresIn: '7d',
+                            issuer: 'sms.dankook.ac.kr',
+                            subject: 'user',
+                        },
+                        (error, token) => {
+                            if (error) res.status(409).json({ error });
+                            // send final user info
+                            res.cookie('x-access-token', token, { httpOnly: true });
+                            res.json({ message: 'Sign-in Successfully', token });
+                        },
+                    );
+                } else {
+                    res.status(409).json({ error: { name: 'invalid' } });
+                }
+            })
+            .catch((error) => res.status(409).json({ error }));
     },
 };
