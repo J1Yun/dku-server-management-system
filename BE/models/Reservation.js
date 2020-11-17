5db9d1a0c3e6e8ecd7dba4e5b73b5edb8a4841c8
zinirun <heojj97@naver.com>
2020-11-17 18:29:47 +0900

    Revert "Update Reservation.js"
    This reverts commit 5466950115dcc380d846ddff7f19f8d6134c1a12.

--- BE/models/Reservation.js
+++ BE/models/Reservation.js
@@ -1,35 +1,41 @@
-const moment = require('moment');
+const moment = require("moment");
 
 module.exports = function (sequelize, DataTypes) {
-    const Reservation = sequelize.define('reservation', {
-        userId: { type: DataTypes.STRING, allowNull: false },
-        serverId: { type: DataTypes.INTEGER, allowNull: false },
-        start: { type: DataTypes.DATEONLY, allowNull: false },
-        end: { type: DataTypes.DATEONLY, allowNull: false },
-        purpose: { type: DataTypes.TEXT, allowNull: false },
-        applyOk: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  const Reservation = sequelize.define("reservation", {
+    userId: { type: DataTypes.STRING, allowNull: false },
+    serverId: { type: DataTypes.INTEGER, allowNull: false },
+    start: { type: DataTypes.DATEONLY, allowNull: false },
+    end: { type: DataTypes.DATEONLY, allowNull: false },
+    purpose: { type: DataTypes.TEXT, allowNull: false },
+    applyOk: { type: DataTypes.INTEGER, allowNull: false , defaultValue: 0 }
+  });
+
+  Reservation.prototype.dateFormat = (date) => moment(date).format("YYYY-MM-DD");
+
+  Reservation.associate = (models) => {
+    Reservation.belongsTo(models.user, {
+        foreignKey: 'userId',
+        targetKey: 'userId',
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
     });
 
-    Reservation.prototype.dateFormat = (date) => moment(date).format('YYYY-MM-DD');
+    Reservation.belongsTo(models.server, {
+      foreignKey: 'serverId',
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
 
-    Reservation.associate = (models) => {
-        Reservation.belongsTo(models.user, {
-            foreignKey: 'userId',
-            targetKey: 'userId',
-            onDelete: 'cascade',
-            onUpdate: 'cascade',
-        });
 
-        Reservation.belongsTo(models.server, {
-            foreignKey: 'serverId',
-            onDelete: 'cascade',
-            onUpdate: 'cascade',
-        });
+  });
 
-        Reservation.hasOne(models.return, {
-            foreignKey: 'reservationId',
-        });
-    };
+    Reservation.hasOne(models.return, {
+      foreignKey: 'reservationId'
+    });
 
-    return Reservation;
+    Reservation.hasMany(models.postpone, {
+      foreignKey: 'reservationId'
+    });
+  };
+
+  return Reservation;
 };
