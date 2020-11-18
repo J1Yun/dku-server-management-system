578b87c5cfca287a74c7a2d354fc2105b2cf7b54
zinirun <heojj97@naver.com>
2020-11-18 15:40:09 +0900

    [FE] Change Header Color

--- FE/src/ClientHeader.js
+++ FE/src/ClientHeader.js
@@ -1,101 +1,101 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router";
-import { AppBar, Toolbar, Button } from "@material-ui/core";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    "& *": {
-      color: "white",
+    root: {
+        flexGrow: 1,
+        '& *': {
+            color: 'white',
+        },
     },
-  },
-  logo: {
-    fontSize: 18,
-  },
-  menuButton: {
-    marginLeft: theme.spacing(1.5),
-    fontSize: 16,
-  },
-  title: {
-    flexGrow: 1,
-  },
-  list: {
-    width: 250,
-  },
-  header: {
-    backgroundColor: "#098be3",
-  },
-  headerContentWrapper: {
-    display: "flex",
-    flexGrow: 1,
-    alignItems: "center",
-  },
-  logoWrapper: {
-    flex: 1,
-  },
-  menuWrapper: {},
+    logo: {
+        fontSize: 18,
+    },
+    menuButton: {
+        marginLeft: theme.spacing(1.5),
+        fontSize: 16,
+    },
+    title: {
+        flexGrow: 1,
+    },
+    list: {
+        width: 250,
+    },
+    header: {
+        backgroundColor: '#0075F6',
+    },
+    headerContentWrapper: {
+        display: 'flex',
+        flexGrow: 1,
+        alignItems: 'center',
+    },
+    logoWrapper: {
+        flex: 1,
+    },
+    menuWrapper: {},
 }));
 
 const menus = [
-  {
-    id: 1,
-    name: "예약하기",
-    link: "/client/apply_reservation",
-  },
-  {
-    id: 2,
-    name: "반납하기",
-    link: "/client/apply_return",
-  },
+    {
+        id: 1,
+        name: '예약하기',
+        link: '/client/apply_reservation',
+    },
+    {
+        id: 2,
+        name: '반납하기',
+        link: '/client/apply_return',
+    },
 
-  {
-    id: 3,
-    name: "예약확인",
-    link: "/client/confirm_reservation",
-  },
-  {
-    id: 4,
-    name: "이용문의",
-    link: "/client/ask",
-  },
+    {
+        id: 3,
+        name: '예약확인',
+        link: '/client/confirm_reservation',
+    },
+    {
+        id: 4,
+        name: '이용문의',
+        link: '/client/ask',
+    },
 ];
 
 export default function ClientHeader() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" className={classes.header}>
-        <Toolbar>
-          <React.Fragment>
-            <div className={classes.headerContentWrapper}>
-              <div className={classes.logoWrapper}>
-                <Button
-                  component={Link}
-                  to="/client"
-                  className={classes.logo}
-                  style={{ color: "white" }}
-                >
-                  단국대학교 서버관리시스템
-                </Button>
-              </div>
-              <div className={classes.menuWrapper}>
-                {menus.map((menu) => (
-                  <Button
-                    className={classes.menuButton}
-                    key={menu.id}
-                    component={Link}
-                    to={menu.link}
-                  >
-                    {menu.name}
-                  </Button>
-                ))}
-              </div>
-            </div>
-          </React.Fragment>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" className={classes.header}>
+                <Toolbar>
+                    <React.Fragment>
+                        <div className={classes.headerContentWrapper}>
+                            <div className={classes.logoWrapper}>
+                                <Button
+                                    component={Link}
+                                    to="/client"
+                                    className={classes.logo}
+                                    style={{ color: 'white' }}
+                                >
+                                    단국대학교 서버관리시스템
+                                </Button>
+                            </div>
+                            <div className={classes.menuWrapper}>
+                                {menus.map((menu) => (
+                                    <Button
+                                        className={classes.menuButton}
+                                        key={menu.id}
+                                        component={Link}
+                                        to={menu.link}
+                                    >
+                                        {menu.name}
+                                    </Button>
+                                ))}
+                            </div>
+                        </div>
+                    </React.Fragment>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
