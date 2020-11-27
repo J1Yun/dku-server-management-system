df8fa5cb189e3df63cb2d5d89b89550101929b69
Choi-Ji-Yun <64777557+Choi-Ji-Yun@users.noreply.github.com>
2020-11-27 21:28:00 +0900

    [BE] Add Table HostServer

--- BE/models/Server.js
+++ BE/models/Server.js
@@ -7,15 +7,26 @@ module.exports = function (sequelize, DataTypes) {
         cpu: { type: DataTypes.INTEGER, allowNull: false },
         ram: { type: DataTypes.INTEGER, allowNull: false },
         location: { type: DataTypes.STRING, allowNull: false },
+        host: { type: DataTypes.STRING, allowNull: false },
+        port: { type: DataTypes.STRING, allowNull: false },
+        instanceName: { type: DataTypes.STRING, allowNull: false },
+        hostId: { type: DataTypes.INTEGER, allowNull: false }
     });
-
     Server.prototype.dateFormat = (date) => moment(date).format('YYYY-MM-DD');
 
     Server.associate = (models) => {
         Server.hasMany(models.reservation, {
             foreignKey: 'serverId',
         });
+
+        Server.belongsTo(models.hostserver, {
+            foreignKey: 'hostId',
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+        });
     };
+
+    
 
     return Server;
 };
