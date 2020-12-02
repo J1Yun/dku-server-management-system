bed65b9e162f272c731c979012204bba3f2eea83
zinirun <heojj97@naver.com>
2020-12-02 15:23:11 +0900

    [BE] Update Crypto Hashing

--- BE/controllers/user/user.ctrl.js
+++ BE/controllers/user/user.ctrl.js
@@ -1,17 +1,57 @@
 const models = require('../../models');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const createSalt = () =>
+    new Promise((resolve, reject) => {
+        crypto.randomBytes(64, (err, buf) => {
+            if (err) reject(err);
+            resolve(buf.toString('base64'));
+        });
+    });
+
+const createHashedPassword = (plainPassword) =>
+    new Promise(async (resolve, reject) => {
+        const salt = await createSalt();
+        crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
+            if (err) reject(err);
+            resolve({ password: key.toString('base64'), salt });
+        });
+    });
+
+const makePasswordHashed = (userId, plainPassword) =>
+    new Promise(async (resolve, reject) => {
+        const salt = await models.user
+            .findOne({
+                attributes: ['salt'],
+                raw: true,
+                where: {
+                    userId,
+                },
+            })
+            .then((result) => result.salt);
+        crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
+            if (err) reject(err);
+            resolve(key.toString('base64'));
+        });
+    });
 
 module.exports = {
-    post_signup: async (req, res) =>
-        await models.user
+    post_signup: async (req, res) => {
+        const { password, salt } = await createHashedPassword(req.body.user.password);
+        return await models.user
             .create({
                 ...req.body.user,
+                password,
+                salt,
             })
             .then((result) => res.json({ userId: result.dataValues.userId }))
-            .catch((error) => res.json({ error })),
+            .catch((error) => res.json({ error }));
+    },
 
     post_signin: async (req, res) => {
-        const { userId, password } = req.body.user;
+        const { userId, password: plainPassword } = req.body.user;
+        const password = await makePasswordHashed(userId, plainPassword);
         const secret = req.app.get('jwt-secret');
 
         return await models.user
--- BE/models/User.js
+++ BE/models/User.js
@@ -4,6 +4,7 @@ module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define('user', {
         userId: { type: DataTypes.STRING, allowNull: false, unique: true },
         password: { type: DataTypes.STRING, allowNull: false },
+        salt: { type: DataTypes.STRING, allowNull: false },
         name: { type: DataTypes.STRING, allowNull: false },
         type: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
         tel: { type: DataTypes.STRING, allowNull: false },
