b6b0fb27cf90a175ce1eadd407719f7caf5117bd
zinirun <heojj97@naver.com>
2020-12-14 17:05:42 +0900

    [BE] Update Promise

--- BE/graphql/ctrl/admin/getServersFromAdmin.js
+++ BE/graphql/ctrl/admin/getServersFromAdmin.js
@@ -1,10 +1,9 @@
 const models = require('../../../models');
-module.exports = async () => {
-    return await models.server
+module.exports = async () =>
+    await models.server
         .findAll({
             attributes: ['id', 'name', 'os', 'cpu', 'ram', 'location'],
             raw: true,
         })
         .then((result) => result)
         .catch((err) => err);
-};
