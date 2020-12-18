ebc9c0e1259ee8ddc14a3bffa50ba9a2ec680a52
zinirun <heojj97@naver.com>
2020-12-18 18:26:27 +0900

    [BE] Update docker

---
+++ BE/build_n_run.sh
@@ -0,0 +1,30 @@
+# Build N Run Container with DB Container
+# 2020. 09. 18 Zini
+
+### You MUST RUN Database Container First ###
+
+docker_username="heojj97"
+container_name="dku-sms-backend"
+image_name="dku-sms-backend"
+db_container_name="dku-sms-database"
+version="0.1"
+#https_port=443
+http_port=80
+
+echo "## Automation docker build and run ##"
+
+# remove container
+echo "=> Remove previous container..."
+docker rm -f ${container_name}
+
+# remove image
+echo "=> Remove previous image..."
+docker rmi -f ${docker_username}/${image_name}:${version}
+
+# new-build/re-build docker image
+echo "=> Build new image..."
+docker build --tag ${docker_username}/${image_name}:${version} .
+
+# Run container connected to existing database container
+echo "=> Run container..."
+docker run -t -d --name ${container_name} -p ${http_port}:${http_port} -p --link ${db_container_name}:db -e DATABASE_HOST=db ${docker_username}/${image_name}:${version} #${https_port}:${https_port}
--- BE/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-dockerize -wait tcp://mysql:3306 -timeout 20s
-
-echo "Start server from docker-entrypoint"
-yarn start
