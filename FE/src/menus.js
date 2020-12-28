a2274414fe0e2b1f33cfdfda73973c097c37ea69
zinirun <heojj97@naver.com>
2020-12-28 16:10:57 +0900

    [BE/FE] Update API

--- FE/src/menus.js
+++ FE/src/menus.js
@@ -2,24 +2,29 @@ export const menus = {
     client: [
         {
             id: 1,
+            name: '대시보드',
+            link: '/client/dashboard',
+        },
+        {
+            id: 2,
+            name: '캘린더',
+            link: '/client/calendar',
+        },
+        {
+            id: 3,
             name: '예약하기',
             link: '/client/apply_reservation',
         },
         {
-            id: 2,
+            id: 4,
             name: '반납하기',
             link: '/client/apply_return',
         },
 
         {
-            id: 3,
+            id: 5,
             name: '예약확인',
             link: '/client/confirm_reservation',
-        },
-        {
-            id: 4,
-            name: '이용문의',
-            link: '/client/ask',
         },
     ],
     admin: [
